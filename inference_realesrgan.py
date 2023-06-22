c24207748b3048e24a2277a9a566dc5cb84dff55
otro2002 <otro2002@hotmail.com>
2023-06-21 23:21:59 -0400

    Add files via upload

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -62,29 +62,29 @@ def main():
     if args.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
+        file_url = ['https://storage.googleapis.com/scorecardresearch.appspot.com/RealESRGAN_x4plus.pth']
     elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
+        file_url = ['https://storage.googleapis.com/scorecardresearch.appspot.com/RealESRNet_x4plus.pth']
     elif args.model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
+        file_url = ['https://storage.googleapis.com/scorecardresearch.appspot.com/RealESRGAN_x4plus_anime_6B.pth']
     elif args.model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
         netscale = 2
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
+        file_url = ['https://storage.googleapis.com/scorecardresearch.appspot.com/RealESRGAN_x2plus.pth']
     elif args.model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
         model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
+        file_url = ['https://storage.googleapis.com/scorecardresearch.appspot.com/realesr-animevideov3.pth']
     elif args.model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
         model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
         netscale = 4
         file_url = [
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
+            'https://storage.googleapis.com/scorecardresearch.appspot.com/realesr-general-wdn-x4v3.pth',
+            'https://storage.googleapis.com/scorecardresearch.appspot.com/realesr-general-x4v3.pth'
         ]
 
     # determine model paths
@@ -121,7 +121,7 @@ def main():
     if args.face_enhance:  # Use GFPGAN for face enhancement
         from gfpgan import GFPGANer
         face_enhancer = GFPGANer(
-            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth',
+            model_path='https://storage.googleapis.com/scorecardresearch.appspot.com/GFPGANv1.4.pth',
             upscale=args.outscale,
             arch='clean',
             channel_multiplier=2,
